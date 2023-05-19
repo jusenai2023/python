ecd4c1501307d420d40e422d7c4cc00639e847e6
Bordinassi <123772388+jusenai2023@users.noreply.github.com>
2023-05-19 11:50:07 -0300

    primeira aula de python

---
+++ print.py
@@ -0,0 +1,34 @@
+print("mensagem que eu quero ", end=".")
+
+print("mensagem 2")
+
+print('fulano é meu "amigo"')
+
+print("fulano é meu \"amigo\"")
+
+print("1+1=2")
+
+print("1 x 1 = ", 1*1)
+
+print("10² =" , 10**3)
+
+print( "10³ =", pow(10,3))
+
+nome = " "
+print(nome)
+nome = 1
+print(nome)
+nome = 1+1
+print(nome)
+nome = True
+print(nome)
+
+nome = "A"
+X =  1
+X = 1+1
+y = True
+
+D1 = 5.5
+D2 = "5"
+print (int(D1 + int(D2)))
+print (round(D1 + int(D2)))
